785743c5f38e5750b8468e85744e73efc413bca8
barmenski <1kiloom@gmail.com>
2021-02-04 21:24:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let str=n+"";
+    let res="";
+    for(let i=str.length-1; i>=0; i--){
+        if((i===0)&&(str[i]==="-"||str[i]==="0")){
+            continue;
+        } else res=res+str[i];
+    }
+    return res;
 }
